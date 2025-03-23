be2d4c405bac3176dd577aaa75dbf92e17ddcc46
salamanderman234 <tresnasaputra9@gmail.com>
2025-03-23 18:54:19 +0800

    feat: reset password & verify user service

--- config/error.config.go
+++ config/error.config.go
@@ -4,26 +4,39 @@ import (
 	"http"
 )
 
-type HttpError struct {
+type Response struct {
 	Status	int
 	Message	string
+	Data 	any
 }
 
-func (h HttpError) Error() string {
+func (h Response) Error() string {
 	return h.Message
 }
 
 var (
-	ErrInvalidCredentials = HttpError{
+	ErrInvalidCredentials = Response{
 		Status:		http.StatusUnauthorized,
 		Message:	"Invalid credentials",
 	},
-	ErrUserBanned = HttpError {
+	ErrUserBanned = Response {
 		Status:		418,
 		Message:	"User is currently banned",
 	},
-	ErrUserSuspended = HttpError {
+	ErrUserSuspended = Response {
 		Status:		419,
-		Message:	"User is currently suspended"
+		Message:	"User is currently suspended",
+	},
+	ErrInvalidKey = Response {
+		Status:		http.StatusBadRequest,
+		Message:	"Invalid key for this action",
+	},
+	ErrKeyExpired = Response {
+		Status:		http.StatusBadRequest,
+		Message:	"This key already expired, please issue a new one !",
+	},
+	ErrBadRequest = Response {
+		Status:		http.StatusBadRequest,
+		Message:	"Bad request",
 	}
 )
--- services/auth.service.go
+++ services/auth.service.go
@@ -9,10 +9,40 @@ import (
 	"github.com/salamanderman234/pos-backend/models"
 	"github.com/salamanderman234/pos-backend/repositories"
 	"github.com/salamanderman234/pos-backend/config"
+	"github.com/salamanderman234/pos-backend/helpers"
 )
 
-func AuthGenerateTwoFactorEncodedString(user models.User) (string, error) {
-	return "", nil
+func AuthDecodeTwoFactorString(encoded string, usingTime bool) (string, string, string, error) {
+	mappedVal, err := helpers.DecodeString(encoded)
+	if len(mappedVal) < 2 {
+		return "", "", "", config.ErrInvalidKey
+	}
+
+	username := mappedVal[0]
+	validKey := mappedVal[1]
+	
+	timeStr := ""
+	if usingTime {
+		if len(mappedVal) < 3 {
+			return "", "", "", config.ErrInvalidKey	
+		}
+		timeStr = mappedVal[2]
+	}	
+	return username, validKey, timeStr, nil
+} 
+
+func AuthEncodeTwoFactorString(username string, until ...time.Time) (string, string, error) {
+	validKey := helpers.GenerateRandomString(6, helpers.UPPERCASE_CHARSET, helpers.NUMBER_CHARSET)
+	arrFormat := []string{
+		username,
+		validKey,
+	}
+	if len(until) >= 1 {
+		timeStr := until[0].Format("2025-03-23 16:02:03")
+		arrFormat = append(arrFormat, timeStr)
+	}
+	result := helpers.EncodeString(arrFormat...)
+	return result, validKey, nil
 }
 
 func AuthGenerateToken(user models.User) (string, error) {
@@ -39,13 +69,12 @@ func AuthLogin(ctx context, username string, password string) (models.User, bool
 		"email",
 		"password",
 		"fullname",
-		"avatar",
 		"banned_at",
 		"ban_reason",
 		"suspended_at",
 		"suspended_until",
 		"suspend_reason",
-		"activated_at",
+		"verified_at",
 		"two_factor_method",
 		"is_two_factor_enabled",
 	}
@@ -75,21 +104,31 @@ func AuthLogin(ctx context, username string, password string) (models.User, bool
 	return user, false, token, nil
 }
 
-func VerififyTwoFactor(ctx context.Context, encoded string, key string) (string, error) {
-	mappedVal, err := helpers.DecodeString(encoded)
+func AuthVerififyTwoFactor(ctx context.Context, encoded string, key string) (string, error) {
+	username, validKey, exp, err := AuthDecodeTwoFactorString(encoded, true)
 	if err != nil {
-		return "", err
+		return "", config.ErrInvalidKey
 	}
-	if len(mappedVal) < 2 {
-		return "", config.ErrBadRequest
+	untilParsed := time.Parse("2025-03-23 15:03:03", exp)
+	if time.Now().After(untilParsed) {
+		return "", config.ErrExpiredKey
 	}
-	method := mappedVal[0]
-	username := mappedVal[1]
-	
+
 	selects := []string{
-	
+		"username",
+		"email",
+		"fullname",
+		"banned_at",
+		"ban_reason",
+		"suspended_at",
+		"suspend_reason",
+		"suspended_until",
+		"verified_at",
+		"two_factor_method",
+		"is_two_factor_enabled",
+		"secret",
 	}
-	preloads := []string{}
+	preloads := []string{"Notifications"}
 	user, err := repositories.UserFindByUsername(ctx, username, selects, preloads)
 	if err != nil {
 		return "", config.ErrInvalidKey
@@ -98,18 +137,13 @@ func VerififyTwoFactor(ctx context.Context, encoded string, key string) (string,
 	if err := AuthCheckUserSuspendBanState(user); err != nil {
 		return "", err
 	}
-
+	if !user.IsTwoFactorEnabled {
+		return "", config.ErrInvalidKey
+	}
+	method := user.TwoFactorMethod
 	switch(method) {
 	case config.TwoFactorEnum_EMAIL:
-		if len(mappedVal) < 4 {
-			return "", config.ErrBadRequest
-		}
-		validKey := mappedVal[2]
-		until := mappedVal[3]
-
-		parsedUntil, _ := time.Parse("2025-03-20 16:03:04", until)
-
-		if validKey != key || time.Now().After(parsedUntil) {
+		if validKey != key {
 			return "", config.ErrInvalidKey		
 		}
 	case config.TwoFactorEnum_GA:
@@ -129,6 +163,85 @@ func VerififyTwoFactor(ctx context.Context, encoded string, key string) (string,
 	return token, nil
 } 
 
-func ResendTwoFactor(ctx context.Context, encoded string) error {
+func AuthResendTwoFactor(ctx context.Context, encoded string) (models.User, string, string, error) {
+	username, _, _, err := AuthDecodeTwoFactorString(encoded, true)
+	if err != nil {
+		return models.User{}, "", "", err
+	}
+	selects := []string{"username", "two_factor_method", "is_two_factor_enabled"}
+	preloads := []string{}
+	user, err := repositories.UserFindByUsername(ctx, username, selects, preloads)
+	if err != nil {
+		return user, "", "", config.ErrInvalidKey
+	}
+	if !user.IsTwoFactorEnabled {
+		return user, "", "", config.ErrInvalidKey
+	}
+	encodedResult, validKey, err := AuthEncodeTwoFactorString(username, time.Now().Add(config.TIME_TWO_FACTOR)
+	
+	return user, encodedResult, validKey, err
+}
 
+func AuthVerifyUser(ctx context.Context, key string, username string) (models.User, error) {
+	nowUnix := time.Now().Unix()
+	selects := []string{
+		"id", 
+		"username", 
+		"key", 
+		"key_valid_until",
+		"verified_at",
+	}
+	preloads := []string{}
+	user, err := models.UserFindByUsername(ctx, username, selects, preloads)
+	if err != nil {
+		return user, config.ErrInvalidKey
+	}
+	if user.VerifiedAt != 0 {
+		return user, config.ErrInvalidKey
+	}
+	if key != user.Key {
+		return user, config.ErrInvalidKey
+	}
+	if nowUnix > user.KeyValidUntil {
+		return user, config.ErrExpiredKey
+	}
+	
+	id := user.ID
+	selects = []string{
+		"key",
+		"key_valid_until",
+		"verified_at",
+	}
+	data := models.User{
+		VerifiedAt: 	nowUnix,
+		Key:		"",
+		KeyValidUntil:	0,
+	}
+	return repositories.UserUpdate(id, data, selects...)
+}
+
+func AuthResetPassword(ctx context.Context, username string, code string, newPassword string) error {
+	nowUnix := time.Now().Unix()
+	selects := []string{
+		"username",
+		"key",
+		"key_valid_until",
+		"key_purpose",
+	}
+
+	preloads := []string{"OldPasswords"}
+	user, err := repositories.UserFindByUsername(ctx, username, selects, preloads)
+	if err != nil {
+		return err
+	}	
+	if user.KeyPurpose != config.UserKeyPurposeEnum_RESET_PASSWORD {
+		return config.ErrInvalidKey
+	}
+	if code != user.Key {
+		return config.ErrInvalidKey
+	}
+	if nowUnix > user.KeyValidUntil {
+		return config.ErrExpiredKey
+	}
+	
 }
